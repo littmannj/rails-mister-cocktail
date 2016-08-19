b8e52a3cf2cc25ec14869183b316ea63eef499d1
jimmy littmann <littmann.j@gmail.com>
2016-08-19 16:23:58 +0100

    done

--- app/models/cocktail.rb
+++ app/models/cocktail.rb
@@ -2,5 +2,4 @@ class Cocktail < ApplicationRecord
   validates :name, presence: true, uniqueness: true
   has_many :doses, dependent: :destroy
   has_many :ingredients, through: :doses
-  mount_uploader :photo, PhotoUploader
 end
