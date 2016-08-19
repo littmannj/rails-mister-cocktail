3e395510dfde469415605092005539ec13273823
jimmy littmann <littmann.j@gmail.com>
2016-08-19 11:05:08 +0100

    sorted delete dose

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -1,7 +1,5 @@
 class DosesController < ApplicationController
-
-  before_action :set_cocktail
-
+  before_action :set_cocktail, only: [:create]
   def create
     @dose = Dose.new(dose_params)
     @dose.cocktail = @cocktail
@@ -11,21 +9,19 @@ class DosesController < ApplicationController
       render "cocktails/show"
     end
   end
-
   def destroy
+    # @cocktail = Cocktail.find(params[:id])
     @dose = Dose.find(params[:id])
+    cocktail_id = @dose.cocktail_id
     @dose.destroy
-    redirect_to cocktail_path(@cocktail)
+    redirect_to cocktail_path(cocktail_id)
   end
 
   private
-
   def set_cocktail
     @cocktail = Cocktail.find(params[:cocktail_id])
   end
-
   def dose_params
     params.require(:dose).permit(:description, :ingredient_id)
   end
-
 end
